d437d1ab74cc4f186bc2c4a0bf6df479e8297c62
huongnhd <huongnhd@lifull-tech.vn>
2019-03-20 10:35:09 +0700

    release version v001

--- setup.py
+++ setup.py
@@ -4,19 +4,36 @@
 from setuptools import setup, find_packages
 setup(
     name='bean',
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(
         '*.py',
         exclude=[
             '*.tests', '*.tests.*', 'tests.*', 'tests'
         ]
     ),
+    license='MIT',
+    description='read xls, xlsx on server',
+    author='huongnhd',
+    author_email = 'huong.nhdh@gmail.com',
+    url = 'https://github.com/huongnhdh/catxls.git',
+    download_url = 'https://github.com/huongnhdh/catxls/archive/v_001.tar.gz',
+    keywords = ['xls', 'xlsx', 'cat xlsx', 'server'],
     install_requires=[
       'pyexcel==0.5.13',
       'pyexcel-xls==0.5.8'
     ],
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build System :: System Shells',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
     entry_points="""
     [console_scripts]
-    catxlsx=app.scripts.catxlsx:_main
+    catxls=app.scripts.catxlsx:_main
     """
 )
